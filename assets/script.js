b97fce8e0f1ae641725b4f0a4883688a0ae26113
Mickaël Douceré <mickamickadou@gmail.com>
2024-05-13 11:14:11 +0200

    Optimisation du code (suppression du "else" et remaniement pour les 2 flèches de navigation et suppression d'une double déclaration)

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 
-// Tableau contenant des objets (partie de la source de l'image & texte lié à la slide)
-// ************************************************************************************
+// Tableau contenant des objets (partie de la source de l'image & texte lié à la slide) Il est possible d'ajouter ou supprimer des slides sans modification du code.
+// *****************************************************************************************************************************************************************
 
 const slides = [
 	{
@@ -92,7 +92,6 @@ function bulletsSelect() {
 function resetBullets() {
 	let bullet = document.querySelectorAll(".dots div")				// Récupération de l'ensemble des éléments DOM "dot" (bullets)
 	for (j = 0; j < bullet.length; j++) {							// Création d'une boucle "for" pour supprimer la classe "dot_selected" de chaque "bullet"
-		let bullet = document.querySelectorAll(".dots div")
 		bullet[j].classList.remove("dot_selected")
 	}
 }
@@ -102,19 +101,13 @@ function resetBullets() {
 																	
 let arrowLeft = document.querySelector(".arrow_left")				// Récupération de l'élément du DOM "img" (flèche gauche)
 arrowLeft.addEventListener("click", () => {							// Ajout d'un écouteur au clic sur la flèche gauche
-	if (i === 0) {													// Structure conditionnelle permettant d'appeler les différentes fonctions dans l'ordre et de gérer le cas particulier pour que le carrousel boucle à l'infini :
-		i = 3														// Comme il n'y a pas de diapo avant la 1ère on passe à la dernière
-		resetBullets()												// Appel de la fonction qui permet de déselectionner les points
+	resetBullets()													// Appel de la fonction qui permet de déselectionner les points
+	i--																// Incrémentation (négative) permettant de passer d'une diapo à l'autre dans le bonne ordre
+	if (i === -1) {													// Structure conditionnelle permettant d'appeler les différentes fonctions dans l'ordre et de gérer le cas particulier pour que le carrousel boucle à l'infini :
+		i = slides.length-1	}										// Comme il n'y a pas de diapo avant la 1ère on passe à la dernière (il y a 4 diapos : la 1érè a pour indice 0 et la dernière 3)
 		slidesDisplay(slides)										// Appel de la fonction qui permet d'afficher la diapo
 		tagLineDisplay(slides)										// Appel de la fonction qui permet d'afficher le slogan
 		bulletsSelect()												// Appel de la fonction qui permet de sélectionner le point
-	} else {
-		resetBullets()
-		i--															// Incrémentation (négative) permettant de passer d'une diapo à l'autre dans le bonne ordre
-		slidesDisplay(slides)
-		tagLineDisplay(slides)
-		bulletsSelect()
-	}
 }
 )
 
@@ -123,18 +116,12 @@ arrowLeft.addEventListener("click", () => {							// Ajout d'un écouteur au cli
 
 let arrowRight = document.querySelector(".arrow_right")				// Idem
 arrowRight.addEventListener("click", () => {
-	if (i === slides.length - 1) {									// Comme il n'y a pas de diapo après la 4ème on retourne à zéro
-		i = 0														// "slides.length - 1" => Permet de prendre en compte l'ensemble des diapos et le fait que l'index d'un tableau commence à [0]
-		resetBullets()
-		slidesDisplay(slides)
+	resetBullets()
+	i++
+	if (i === slides.length ) {										// Comme il n'y a pas de diapo après la 4ème on retourne à zéro
+		i = 0	}													// "slides.length" => Permet de prendre en compte l'ensemble des diapos et le fait que l'index d'un tableau commence à [0]
+		slidesDisplay(slides)										// Il y a 4 diapos, la dernière a pour index 3 ; du coup à l'index 4 on retourne à l'index 0
 		tagLineDisplay(slides)
 		bulletsSelect()
-	} else {
-		resetBullets()
-		i++
-		slidesDisplay(slides)
-		tagLineDisplay(slides)
-		bulletsSelect()
-	}
 }
 )
